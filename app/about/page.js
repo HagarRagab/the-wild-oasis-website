174190fd926453d94380424f1d771b6fc83747b6
HagarRagab <101262480+HagarRagab@users.noreply.github.com>
2025-03-20 13:49:43 +0200

    initial commit

---
+++ app/about/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div>
+            <h2>About wild oasis page</h2>
+        </div>
+    );
+}
---
+++ app/cabins/page.js
@@ -0,0 +1,7 @@
+export default async function Page() {
+    return (
+        <div>
+            <h2>Cabins page</h2>
+        </div>
+    );
+}
